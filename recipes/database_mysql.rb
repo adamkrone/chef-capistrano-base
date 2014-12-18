4b7310caa5d7e6963ac006e378f5ea3732ffcca4
Adam Krone <krone.adam@gmail.com>
2014-12-18 14:03:23 -0600

    Issue #3 - Moves to new MySQL cookbook
    It appears to install correctly, however it looks like this will
    have to wait until the database cookbook supports the new version.

--- recipes/database_mysql.rb
+++ recipes/database_mysql.rb
@@ -7,8 +7,10 @@
 #
 #
 
+mysql_root = Chef::DataBagItem.load('mysql', 'root')
+
 capistrano_mysql node['capistrano_base']['db']['name'] do
-  mysql_root_password node['mysql']['server_root_password']
+  mysql_root_password mysql_root['password']
   db_user node['capistrano_base']['db']['user']
   db_password node['capistrano_base']['db']['user_password']
   db_environments node['capistrano_base']['db']['environments']
